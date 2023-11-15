bbb4525b56090c7b44984a2fe910e24464783353
Berti  Salom <bertisalom@Bertis-MacBook-Air.local>
2023-11-15 21:00:38 +0300

    Add macro to convert amount

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,10 @@
 select
     id as payment_id,
-    orderid as order_id,
+    orderid as order_id,	
     paymentmethod as payment_method,
     status,
-    amount / 100 as amount,
-    created as created_at
 
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at,	
 from {{ source('stripe', 'payment') }}
-
-
